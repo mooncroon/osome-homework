1b7418f4c9128e99168281dc51b6f5e3f1b70087
croft <croft@fixmost.com>
2022-02-13 18:49:46 +0300

    eslint nav bar

--- fragments/navigation_bar.js
+++ fragments/navigation_bar.js
@@ -2,11 +2,11 @@ const { I } = inject();
 
 module.exports = {
 
-    root: '//nav[@data-test="NavigationBar"]',
-    item: (buttonName)=> `${module.exports.root}//*[@data-test="${buttonName}"]`,
+  root: '//nav[@data-test="NavigationBar"]',
+  item: (buttonName) => `${module.exports.root}//*[@data-test="${buttonName}"]`,
 
-    menuList: {
-      billing: 'a#header-link-billing'
-    }
+  menuList: {
+    billing: 'a#header-link-billing',
+  },
 
-}
+};
